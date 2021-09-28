20b4aa05653167f99fefdf51de88eb6ee5a31645
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-28 14:10:06 +0800

    code opt

--- Anima_08 - Gradient/GradientAnimation/ViewController.swift
+++ Anima_08 - Gradient/GradientAnimation/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
     }
     
     // MARK: - Status Bar
